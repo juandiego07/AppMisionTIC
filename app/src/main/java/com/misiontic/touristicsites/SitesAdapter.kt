c85b57c266c22cef2ae6f772afae9a4276e49a43
juandiego07 <juan_diego07@hotmail.com>
2021-11-20 13:04:55 -0500

    Se culmina adapter

--- app/src/main/java/com/misiontic/touristicsites/SitesAdapter.kt
+++ app/src/main/java/com/misiontic/touristicsites/SitesAdapter.kt
@@ -1,22 +1,38 @@
 package com.misiontic.touristicsites
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+// import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
 import androidx.recyclerview.widget.RecyclerView
 
 class SitesAdapter(val sites: List<DataSites>) : RecyclerView.Adapter<SitesAdapter.SitesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SitesViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SitesViewHolder(layoutInflater.inflate(R.layout.sites_list, parent, false))
     }
 
     override fun onBindViewHolder(holder: SitesViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       holder.render(sites[position])
     }
 
     override fun getItemCount(): Int = sites.size
 
-    class SitesViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    class SitesViewHolder (val view:View) : RecyclerView.ViewHolder(view) {
+        fun render( site: DataSites) {
+            view.findViewById<TextView>(R.id.tvNombre).text = site.nombre
+            view.findViewById<TextView>(R.id.tvDescripcion).text = site.descripcion
+            view.findViewById<TextView>(R.id.tvRanking).text = site.ranking
+            Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(view.findViewById<ImageView>(R.id.ivImg))
+            //Glide.with(context)
+            //    .load(site.imagen)
+            //    .into(view.findViewById<ImageView>(R.id.ivImg))
+        }
 
     }
 }
