ad7ae400c2315bfa48ddc015ffdde78046d5fac0
juandiego07 <juan_diego07@hotmail.com>
2021-11-20 10:42:04 -0500

    Se crea clase modelo para datos del sitio

---
+++ app/src/main/java/com/misiontic/touristicsites/DataSites.kt
@@ -0,0 +1,12 @@
+package com.misiontic.touristicsites
+
+data class DataSites(
+        val nombre: String,
+        val imagen: String,
+        val descripcion: String,
+        val resumen: String,
+        val ranking: String,
+        val ubicacion: String,
+        val temperatura: String,
+        val recomendados: String
+)
