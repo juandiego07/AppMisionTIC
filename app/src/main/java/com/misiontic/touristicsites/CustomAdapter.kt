690548df74c9667eea856d64eedc2b1d75acdcc6
jmendezr85 <jorgemendezr85@gmail.com>
2021-11-23 20:20:15 -0500

    creaion del Splash screnn

--- app/src/main/java/com/misiontic/touristicsites/CustomAdapter.kt
+++ app/src/main/java/com/misiontic/touristicsites/CustomAdapter.kt
@@ -1,65 +1,34 @@
 package com.misiontic.touristicsites
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter(val sites: List<ModelSites>, val context: Context) :
+    RecyclerView.Adapter<CustomAdapter.SitesHolder>() {
 
-    val images = arrayOf(
-        R.drawable.dubai_1,
-        R.drawable.dubai_2,
-        R.drawable.dubai_3,
-        R.drawable.dubai_4,
-        R.drawable.dubai_5)
-
-    val titles = arrayOf(
-        "Dubái creek, Zona Antigua",
-        "Dubái Mall",
-        "Fuente de Dubái",
-        "Safari en el Desierto Dedilay",
-        "Playa Burj Al Arab")
-
-    val description = arrayOf(
-        "Dubái Creek es el corazón de la ciudad",
-        "El Dubái Mall es el Centro Comercial más grande del mundo",
-        "No puedes perderte el show de aguas danzantes de la Fuente de Dubái",
-        "Recorre las arenas rojizas en un Safari por el Desierto de Dubái",
-        "Incursiona en el surf con las olas de la Playa Burj Al Arab")
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-        val v= LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
-        return ViewHolder(v)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.SitesHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SitesHolder(layoutInflater.inflate(R.layout.card_layout, parent, false), context)
     }
 
-    override fun onBindViewHolder(objeto: ViewHolder, i: Int) {
-        objeto.itemImage.setImageResource(images[i])
-        objeto.itemTitle.text=titles[i]
-        objeto.itemDescription.text=description[i]
+    override fun onBindViewHolder(holder: CustomAdapter.SitesHolder, position: Int) {
+        holder.render(sites[position])
     }
 
-    override fun getItemCount(): Int {
-        return titles.size
-    }
+    override fun getItemCount(): Int = sites.size
 
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        var itemImage: ImageView
-        var itemTitle: TextView
-        var itemDescription: TextView
+    class SitesHolder(val view: View, val context: Context) : RecyclerView.ViewHolder(view) {
 
-        init {
-            itemImage = itemView.findViewById(R.id.item_image)
-            itemTitle = itemView.findViewById(R.id.item_title)
-            itemDescription = itemView.findViewById(R.id.item_description)
-
+        fun render(site: ModelSites) {
+            view.findViewById<TextView>(R.id.item_title).text = site.titulo
+            view.findViewById<TextView>(R.id.item_description).text = site.descripcion_corta
+            view.findViewById<TextView>(R.id.item_score).text = "Puntaje: "+site.score
+            Glide.with(context).load(site.ruta_imagen).into(view.findViewById(R.id.item_image))
         }
-
     }
-
-
-
-
 }
--- app/src/main/java/com/misiontic/touristicsites/MainActivity2.kt
+++ app/src/main/java/com/misiontic/touristicsites/MainActivity2.kt
@@ -2,19 +2,66 @@ package com.misiontic.touristicsites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import org.json.JSONException
+import java.io.IOException
 
 class MainActivity2 : AppCompatActivity() {
+
+    private var modelSites = arrayListOf<ModelSites>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
         setContentView(R.layout.activity_main2)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val adapter = CustomAdapter()
-
+        val adapter = CustomAdapter(modelSites, this)
+        initDataJson()
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
+    }
+
+    private fun initDataJson() {
+        val dataJson = readDataJson()
+        try {
+            val dataSites = JSONArray(dataJson)
+            for (i in 0 until dataSites.length()) {
+                val dataSite = dataSites.getJSONObject(i)
+                val site = ModelSites(
+                    titulo = dataSite.getString("titulo"),
+                    descripcion = dataSite.getString("descripcion"),
+                    descripcion_corta = dataSite.getString("descripcion_corta"),
+                    ubicacion = dataSite.getString("ubicacion"),
+                    tiempo = dataSite.getString("tiempo"),
+                    sitios = dataSite.getString("sitios"),
+                    ruta_imagen = dataSite.getString("ruta_imagen"),
+                    score = dataSite.getString("score")
+                )
+                modelSites.add(site)
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun readDataJson(): String? {
+
+        var dataJson: String? = null
+
+        try {
+            val dataRead = assets.open("mock_ciudades.json")
+            val size = dataRead.available()
+            val dataCache = ByteArray(size)
+            dataRead.read(dataCache)
+            dataRead.close()
+            dataJson = String(dataCache)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return dataJson
     }
 }
