2b427783adf1583ea291adb2ab64235949297759
unknown <you@example.com>
2021-12-01 18:25:37 -0500

    copiando de JMaflaNavigation

--- app/src/main/java/com/misiontic/touristicsites/Detail_fragment.kt
+++ app/src/main/java/com/misiontic/touristicsites/Detail_fragment.kt
@@ -3,17 +3,15 @@ package com.misiontic.touristicsites
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 
 class Detail_fragment : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_fragment)
-
-
-
         val objectSite: Intent = intent
         Glide.with(this).load(objectSite.getStringExtra("ruta_imagen")).into(findViewById<ImageView>(R.id.iv_dubai_1))
         findViewById<TextView>(R.id.tvDubai).text = objectSite.getStringExtra("titulo")
@@ -21,16 +19,5 @@ class Detail_fragment : AppCompatActivity() {
         findViewById<TextView>(R.id.tv_detalle2).text = objectSite.getStringExtra("ubicacion")
         findViewById<TextView>(R.id.tv_detalle3).text = objectSite.getStringExtra("tiempo")
         findViewById<TextView>(R.id.sitios).text = objectSite.getStringExtra("sitios")
-
-
-        val boton = findViewById<ImageButton>(R.id.ib_back)
-
-      boton.setOnClickListener {
-
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
-
-
     }
 }
--- app/src/main/java/com/misiontic/touristicsites/PreferenceFragment.kt
+++ app/src/main/java/com/misiontic/touristicsites/PreferenceFragment.kt
@@ -6,34 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageButton
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 
 class PreferenceFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preference, container, false)
+        val view =inflater.inflate(R.layout.fragment_preference, container, false)
+
+        //val button = view.findViewById<Button>(R.id.ib_back)
+        // button.setOnClickListener {
+        //Navigation.findNavController(view).navigate(R.id.action_preferenceFragment_to_list_Fragment2)
+
+        //}
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val button = view.findViewById<ImageButton>(R.id.ib_back)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_preferenceFragment_to_list_Fragment)
-
-        }
-    }
 
 
 }
