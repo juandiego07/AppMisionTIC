b819c423de33ab8ebd08df702868f7707dc69b20
juandiego07 <juan_diego07@hotmail.com>
2021-12-18 04:11:38 -0500

    Se eliminan comentarios del codigo y se aplica formato

--- app/src/main/java/com/misiontic/touristicsites/Detail_fragment.kt
+++ app/src/main/java/com/misiontic/touristicsites/Detail_fragment.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.*
 import com.bumptech.glide.Glide
 
@@ -14,8 +13,6 @@ class Detail_fragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_fragment)
-
-
         val objectSite: Intent = intent
         Glide.with(this).load(objectSite.getStringExtra("ruta_imagen"))
             .into(findViewById<ImageView>(R.id.iv_dubai_1))
@@ -24,27 +21,23 @@ class Detail_fragment : AppCompatActivity() {
         findViewById<TextView>(R.id.tv_detalle2).text = objectSite.getStringExtra("ubicacion")
         findViewById<TextView>(R.id.tv_detalle3).text = objectSite.getStringExtra("tiempo")
         findViewById<TextView>(R.id.sitios).text = objectSite.getStringExtra("sitios")
-
-
         val boton = findViewById<ImageButton>(R.id.ib_back)
 
         boton.setOnClickListener {
-
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-            val button = findViewById<Button>(R.id.Geo)
-            //Log.d("Valor", "Entree0")
-            button.setOnClickListener {
-            //Log.d("Valor", "Entree1")
-            launchMap(objectSite.getStringExtra("lat").toString(), objectSite.getStringExtra("long").toString())
-
-
-
-            }
-
+        val button = findViewById<Button>(R.id.Geo)
+        button.setOnClickListener {
+            launchMap(
+                objectSite.getStringExtra("lat").toString(),
+                objectSite.getStringExtra("long").toString()
+            )
         }
+
+    }
+
     private fun launchMap(lat: String, long: String) {
         val geo = "geo: $lat, $long"
         val gmmIntentUri = Uri.parse(geo)
@@ -52,8 +45,6 @@ class Detail_fragment : AppCompatActivity() {
         mapIntent.setPackage("com.google.android.apps.maps")
         startActivity(mapIntent)
     }
-
-
 
 
 }
--- app/src/main/java/com/misiontic/touristicsites/PreferenceFragment.kt
+++ app/src/main/java/com/misiontic/touristicsites/PreferenceFragment.kt
@@ -5,22 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 
 class PreferenceFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =inflater.inflate(R.layout.fragment_preference, container, false)
-
-
+        val view = inflater.inflate(R.layout.fragment_preference, container, false)
         return view
     }
 
@@ -29,10 +24,8 @@ class PreferenceFragment : Fragment() {
 
         val button = view.findViewById<ImageButton>(R.id.ib_back)
         button.setOnClickListener {
-        Navigation.findNavController(view).navigate(R.id.action_preferenceFragment_to_list_Fragment2)
-
+            Navigation.findNavController(view)
+                .navigate(R.id.action_preferenceFragment_to_list_Fragment2)
         }
     }
-
-
 }
