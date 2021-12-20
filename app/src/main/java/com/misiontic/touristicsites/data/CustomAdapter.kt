fea6de129a535f4570d79322e85a8e3b264c093c
juandiego07 <juan_diego07@hotmail.com>
2021-12-19 20:37:15 -0500

    Se corrigen warning del CustomAdapter.kt

--- app/src/main/java/com/misiontic/touristicsites/data/CustomAdapter.kt
+++ app/src/main/java/com/misiontic/touristicsites/data/CustomAdapter.kt
@@ -1,5 +1,6 @@
 package com.misiontic.touristicsites.data
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.misiontic.touristicsites.R
 
-class CustomAdapter(val sites: List<ModelSites>, val context: Context, val click: (ModelSites?) -> Unit) :
+class CustomAdapter(private val sites: List<ModelSites>, private val context: Context, val click: (ModelSites?) -> Unit) :
     RecyclerView.Adapter<CustomAdapter.SitesHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SitesHolder {
@@ -23,7 +24,7 @@ class CustomAdapter(val sites: List<ModelSites>, val context: Context, val click
 
     override fun getItemCount(): Int = sites.size
 
-    inner class SitesHolder(val view: View, val context: Context) : RecyclerView.ViewHolder(view) {
+    inner class SitesHolder(private val view: View, private val context: Context) : RecyclerView.ViewHolder(view) {
         private var city : ModelSites? = null
         init {
             view.setOnClickListener {
@@ -31,6 +32,7 @@ class CustomAdapter(val sites: List<ModelSites>, val context: Context, val click
             }
         }
 
+        @SuppressLint("SetTextI18n")
         fun render(site: ModelSites) {
             city = site
             view.findViewById<TextView>(R.id.item_title).text = site.titulo
